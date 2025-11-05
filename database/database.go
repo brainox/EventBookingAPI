cbddc5b15963baed7d2bc75f313b849a62c84cf0
Obinna <macmartins081@gmail.com>
2025-11-05 12:35:53 +0400

    created user successfully

--- database/database.go
+++ database/database.go
@@ -40,7 +40,7 @@ func createTables() {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		datetime DATETIME NOT NULL,
-		user_id INTEGER
+		user_id INTEGER,
 		FOREIGN KEY(user_id) REFERENCES users(id)
 	);`
 	_, err = DB.Exec(createEventTable)
